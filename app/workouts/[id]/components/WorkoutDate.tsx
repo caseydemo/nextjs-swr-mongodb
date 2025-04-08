d24ebe5b8cc21175d012158fded3f11620e0698e
Casey DeMattina <cdemattina@gmail.com>
2025-04-08 16:02:01 -0400

    basic workout date component logic

--- app/workouts/[id]/components/WorkoutDate.tsx
+++ app/workouts/[id]/components/WorkoutDate.tsx
@@ -1,11 +1,48 @@
-export default function WorkoutDate({ started, isEditing, handleDateEdit }: { started: string; isEditing: boolean; handleDateEdit: () => void; }) {
+export default function WorkoutDate({
+	started,
+	isEditing,
+	handleDateEdit,
+}: {
+	started: string;
+	isEditing: boolean;
+	handleDateEdit: () => void;
+}) {
+	// this component will receive the 'started' prop, and also an isEditing prop and a handleDateEdit prop
 
-    // this component will receive the 'started' prop, and also an isEditing prop and a handleDateEdit prop
-
-    return (
-        <>
-        teh date:
-        {started}
-        </>
-    )
+	return (
+		<>
+			<div className='row g-3 mb-3'>
+				<div className='col-auto'>
+					<label
+						htmlFor='workout-date'
+						className='form-label'
+					>
+						The Date:
+					</label>
+				</div>
+				<div className='col-auto'>
+					{isEditing ? (
+						<input
+							type='date'
+							id='workout-date'
+							className='form-control'
+							defaultValue={started}
+						/>
+					) : (
+						<span>{started}</span>
+					)}
+				</div>
+				{isEditing && (
+					<div className='col-auto'>
+						<button
+							className='btn btn-primary'
+							onClick={handleDateEdit}
+						>
+							Save
+						</button>
+					</div>
+				)}
+			</div>
+		</>
+	);
 }
